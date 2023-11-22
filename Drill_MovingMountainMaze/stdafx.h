53832d904bbad70b665295708e336e93461a093f
suminchoi6327@gmail.com <suminchoi6327@gmail.com>
2023-11-22 12:53:05 +0900

    초기 커밋.

---
+++ Drill_MovingMountainMaze/stdafx.h
@@ -0,0 +1,24 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <math.h>
+#include <stdio.h>
+#include <algorithm>
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "freeglut.lib")
+
+using namespace std;
